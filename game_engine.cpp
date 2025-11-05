0ceb3b8aaf73e72abf2743ea94080f706460b8ad
EugenPlesca <eugenplesca29@gmail.com>
2025-11-05 17:22:50 +0200

    Lab04

--- game_engine.cpp
+++ game_engine.cpp
@@ -1,66 +1,79 @@
 #include "game_engine.hpp"
+#include "point.hpp"
 #include <iostream>
+#include <fstream>
+#include <algorithm>
 
 GameEngine::GameEngine()
-    : _currentPlayer(Player::X)
-{
-}
+    : _board(std::make_shared<Board>()), _painter(nullptr), _currentPlayer(Player::X) {}
+
+GameEngine::GameEngine(std::shared_ptr<Painter> painter)
+    : _board(std::make_shared<Board>()), _painter(painter), _currentPlayer(Player::X) {}
 
 void GameEngine::Init() {
-    _board = Board();
+    _board = std::make_shared<Board>();
     _currentPlayer = Player::X;
 }
 
 void GameEngine::Run() {
     Init();
+    std::ofstream log("game_log.txt", std::ios::app);
+    int lines[3] = {0, 1, 2};
+
     while (true) {
+        if (_painter) _painter->DrawBoard(*_board);
+
         int x, y;
-        std::cout << "Player " << (_currentPlayer == Player::X ? "X" : "O") << ", enter coordinates (x y): ";
+        std::cout << "Player " << (_currentPlayer==Player::X?"X":"O") << ", enter coordinates (x y): ";
         std::cin >> x >> y;
 
-        Point pos{x, y};
-        if (!_board.PlaceMove(pos, _currentPlayer)) {
-            std::cout << "Invalid move, try again.\n";
-            continue;
-        }
+        Point pos{x,y};
+        if (!_board->PlaceMove(pos,_currentPlayer)) continue;
+
+        log << "Player " << (_currentPlayer==Player::X?"X":"O") << " -> (" << x << "," << y << ")\n";
 
         Player winner = CheckWinner();
         if (winner != Player::None) {
-            std::cout << "Player " << (winner == Player::X ? "X" : "O") << " wins!\n";
+            if (_painter) _painter->DrawBoard(*_board);
+            if (_painter) _painter->ShowWinner(winner);
+            log << "Winner: Player " << (winner==Player::X?"X":"O") << "\n\n";
             break;
         }
 
-        if (_board.IsFull()) {
-            std::cout << "It's a draw!\n";
+        if (_board->IsFull()) {
+            if (_painter) _painter->DrawBoard(*_board);
+            if (_painter) _painter->ShowWinner(Player::None);
+            log << "Result: Draw\n\n";
             break;
         }
 
-        _currentPlayer = (_currentPlayer == Player::X) ? Player::O : Player::X;
+        _currentPlayer = (_currentPlayer==Player::X?Player::O:Player::X);
     }
+
+    log.close();
 }
 
 Player GameEngine::CheckWinner() const {
-    for (int i = 0; i < 3; i++) {
-        if (_board.GetCell({i, 0}) != Player::None &&
-            _board.GetCell({i, 0}) == _board.GetCell({i, 1}) &&
-            _board.GetCell({i, 1}) == _board.GetCell({i, 2}))
-            return _board.GetCell({i, 0});
+    int lines[3] = {0,1,2};
+    for (int i = 0; i < 3; ++i) {
+        if (_board->GetCell({i,0}) != Player::None &&
+            std::all_of(std::begin(lines), std::end(lines), [&](int j){ return _board->GetCell({i,j}) == _board->GetCell({i,0}); }))
+            return _board->GetCell({i,0});
 
-        if (_board.GetCell({0, i}) != Player::None &&
-            _board.GetCell({0, i}) == _board.GetCell({1, i}) &&
-            _board.GetCell({1, i}) == _board.GetCell({2, i}))
-            return _board.GetCell({0, i});
+        if (_board->GetCell({0,i}) != Player::None &&
+            std::all_of(std::begin(lines), std::end(lines), [&](int j){ return _board->GetCell({j,i}) == _board->GetCell({0,i}); }))
+            return _board->GetCell({0,i});
     }
 
-    if (_board.GetCell({0, 0}) != Player::None &&
-        _board.GetCell({0, 0}) == _board.GetCell({1, 1}) &&
-        _board.GetCell({1, 1}) == _board.GetCell({2, 2}))
-        return _board.GetCell({0, 0});
+    if (_board->GetCell({0,0}) != Player::None &&
+        _board->GetCell({0,0}) == _board->GetCell({1,1}) &&
+        _board->GetCell({1,1}) == _board->GetCell({2,2}))
+        return _board->GetCell({0,0});
 
-    if (_board.GetCell({0, 2}) != Player::None &&
-        _board.GetCell({0, 2}) == _board.GetCell({1, 1}) &&
-        _board.GetCell({1, 1}) == _board.GetCell({2, 0}))
-        return _board.GetCell({0, 2});
+    if (_board->GetCell({0,2}) != Player::None &&
+        _board->GetCell({0,2}) == _board->GetCell({1,1}) &&
+        _board->GetCell({1,1}) == _board->GetCell({2,0}))
+        return _board->GetCell({0,2});
 
     return Player::None;
 }
--- painter.cpp
+++ painter.cpp
@@ -1,27 +1,26 @@
 #include "painter.hpp"
-#include <iostream>
+#include <sstream>
 
-class ConsolePainter : public Painter {
-public:
-    void DrawBoard(const Board& board) override {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                Player cell = board.GetCell({i, j});
-                char symbol = ' ';
-                if (cell == Player::X) symbol = 'X';
-                else if (cell == Player::O) symbol = 'O';
-                std::cout << symbol;
-                if (j < 2) std::cout << " | ";
-            }
-            std::cout << "\n";
-            if (i < 2) std::cout << "---------\n";
+void ConsolePainter::DrawBoard(const Board& board) {
+    std::ostringstream output;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            Player cell = board.GetCell({i, j});
+            char symbol = ' ';
+            if (cell == Player::X) symbol = 'X';
+            else if (cell == Player::O) symbol = 'O';
+            output << symbol;
+            if (j < 2) output << " | ";
         }
+        output << "\n";
+        if (i < 2) output << "---------\n";
     }
+    std::cout << output.str();
+}
 
-    void ShowWinner(Player winner) override {
-        if (winner == Player::None)
-            std::cout << "Draw!\n";
-        else
-            std::cout << "Winner: " << (winner == Player::X ? "X" : "O") << "\n";
-    }
-};
+void ConsolePainter::ShowWinner(Player winner) {
+    if (winner == Player::None)
+        std::cout << "Draw!\n";
+    else
+        std::cout << "Winner: " << (winner == Player::X ? "X" : "O") << "\n";
+}
